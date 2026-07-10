b9c9fa0455504e55f034080f2fae766f522b6d62
Muhammad Usman <theusmanriaz1321@gmail.com>
2026-07-10 09:11:30 +0500

    Updated server with all routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const OTP = require('../models/OTP'); // ✅ Add this
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer'); // ✅ Add this
