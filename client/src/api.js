1f415b58b46aad19cc7b1b740f7165f42653314d
Muhammad Usman <theusmanriaz1321@gmail.com>
2026-07-10 10:29:59 +0500

    api.js updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://fast-food-web-ruddy.vercel.app/api';
 
 const api = axios.create({
     baseURL: API_URL,
